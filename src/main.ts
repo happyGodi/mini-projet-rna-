a5987e6d290c66a7e4e6d465b241a4cd86a477a5
happyGodi <godiramaru0@gmail.com>
2024-04-19 09:58:20 +0100

    lang setting done with i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,23 @@ import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import i18next from 'i18next'
+import i18nextVue from 'i18next-vue'
+import en from './lang/en.json'
 import App from './App.vue'
 import router from './router'
+
+
+i18next.init({
+    resources : {
+        en : en
+    }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18nextVue, { i18next})
 
 app.mount('#app')
