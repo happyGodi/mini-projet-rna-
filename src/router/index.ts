de6182b4e5309472bfb5dea2c3ab43a071a23076
happyGodi <godiramaru0@gmail.com>
2024-06-01 06:06:34 +0100

    chart layout corrected, setp 1 done

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,12 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
+    }/* ,
     {
       path: '/traceXY',
       name: 'Tracée de Yn en fonction de Xn',
       component: () => import("../components/TraceXY.vue")
-    }
+    } */
   ]
 })
 
