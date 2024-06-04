8a1418417b96ced47652621ef8804b48d6ad8559
happyGodi <godiramaru0@gmail.com>
2024-06-04 12:08:58 +0100

    training function done, the training error left

--- src/classes/NeuralNetwork.ts
+++ src/classes/NeuralNetwork.ts
@@ -10,7 +10,7 @@ export class NeuralNetwork {
     private weightsInputHidden: number[][] //poids des unités caché venant des entréés
     private weightsOutputHidden: number[][] //poids des unités caché vers la sortie
     private timeSerie: number[]// la série temporelle initiale
-    private prototype: Prototype[][] //les prototypes
+    private prototype: Prototype[] //les prototypes
 
     constructor(inputUnitNumber: number, hiddenUnitNumber: number, outputUnitNumber: number, learningRate: number, timeSerie: number[]) {
         this.inputUnitNumber = inputUnitNumber
@@ -20,32 +20,25 @@ export class NeuralNetwork {
         this.timeSerie = timeSerie
 
         //step1: initialisation des poids avec de petites valeurs aléatoire (min: 0.1, max: 0.5)
-        this.weightsInputHidden = Array.from({length: hiddenUnitNumber}, () => Array.from({length: inputUnitNumber}, () => Math.random() * (0.5 - 0.1) + 0.1))
-        this.weightsOutputHidden = Array.from({length: outputUnitNumber}, () => Array.from({length: hiddenUnitNumber}, () => Math.random() * (0.5 - 0.1) + 0.1))
+        this.weightsInputHidden = Array.from({length: hiddenUnitNumber}, () => Array.from({length: inputUnitNumber}, () => parseFloat((Math.random() * (0.5 - 0.1) + 0.1).toPrecision(8))))
+        this.weightsOutputHidden = Array.from({length: outputUnitNumber}, () => Array.from({length: hiddenUnitNumber}, () => parseFloat((Math.random() * (0.5 - 0.1) + 0.1).toPrecision(8))))
 
         //step2: initialisation du premier prototype
-        this.prototype = this.generatePrototype(timeSerie.slice(0, 300), 6)
+        this.prototype = this.generatePrototype(timeSerie.slice(0, 300))
     }
     //géneration des prototypes
-    private generatePrototype(arr: number[], numberPerSet: number): Prototype[][] {
-        const totalValue = arr.length
-        const prototypeSets = totalValue / numberPerSet
-        const maxValue = numberPerSet * prototypeSets
+    private generatePrototype(arr: number[]): Prototype[] {
+        const prototype: Prototype[] = [];
+        let count = 0;
 
-        let prototype: Prototype[][] = []
-        let count = 0
-
-        for (let i = 0; i < prototypeSets && count < maxValue; i++) {
-            const prototypeSet: Prototype[] = []
-            for (let j = 0; j < numberPerSet && count < maxValue; j++) {
-                const inputs = arr.slice(count, count + this.inputUnitNumber);
-                const target = arr[count + this.inputUnitNumber]
-                prototypeSet.push({inputs, target})
-                count += this.inputUnitNumber + 1
-            }
-            prototype.push(prototypeSet)
+        while (count < arr.length - this.inputUnitNumber) {
+            const inputs = arr.slice(count, count + this.inputUnitNumber);
+            const target = arr[count + this.inputUnitNumber];
+            prototype.push({ inputs, target });
+            count += this.inputUnitNumber + 1;
         }
-        return prototype
+
+        return prototype;
     }
     //fonction d'activation sigmoid
     private sigmoid(x: number): number {
@@ -57,7 +50,7 @@ export class NeuralNetwork {
     }
 
     //step 3: propagation du signal vers l'avant
-    private forwardPropagation(inputs: number[]): { hidden: number[], output: number[] } {
+    private forwardPropagation(inputs: number[]): { hidden: number[], output: number } {
         const hidden = this.weightsInputHidden.map(hiddenWeights => this.sigmoid(hiddenWeights.reduce((sum, weight, i) => parseFloat((sum + weight * inputs[i]).toPrecision(8)), 0)))
         /**
          parcours de l'ensemble de poids entre la couche d'entréé et celle caché
@@ -65,7 +58,8 @@ export class NeuralNetwork {
          la valeur finale est celle retourné par cette fonction
          le processus est le même pour l'instruction suivante mais utilisant la couche caché et celle de sortie
          */
-        const output = this.weightsOutputHidden.map(outputWeights => this.sigmoid(outputWeights.reduce((sum, weight, i) => parseFloat((sum + weight * hidden[i]).toPrecision(8)), 0)))
+        // const output = this.weightsOutputHidden.map(outputWeights => this.sigmoid(outputWeights.reduce((sum, weight, i) => parseFloat((sum + weight * hidden[i]).toPrecision(8)), 0)))
+        const output = this.sigmoid(this.weightsOutputHidden[0].reduce((sum, weight, i) => parseFloat((sum + weight * hidden[i]).toPrecision(8)), 0));
         //calcul de toutes les sortie
         return {hidden, output}
     }
@@ -73,27 +67,59 @@ export class NeuralNetwork {
     //propagation vers l'arrière
     private backwardPropagation(inputs: number[], hiddens: number[], output: number, target: number): void {
         //calcule de l'erreur de sortie
-        const outputError = target - output
+        const outputError = parseFloat((target - output).toPrecision(8))
         //step 4: calcul des deltas pour la couche de sortie
-        const outputDelta = outputError * this.sigmoidDerivated(output)
+        const outputDelta = parseFloat((outputError * this.sigmoidDerivated(output)).toPrecision(8))
         //step 5: propagation des erreurs vers l'arrière et calcul des deltas pour les couches précedentes
         const hiddenErrors = this.weightsOutputHidden[0].map((weight) => parseFloat((weight * outputDelta).toPrecision(8)))
         const hiddenDeltas = hiddenErrors.map((error, i) => parseFloat((error * this.sigmoidDerivated(hiddens[i])).toPrecision(8)))
 
         //step 6: mise à jour de toutes les connexions
+        //mis à jour des connexion entre la couche caché et celle de sortie
         for (let i = 0; i < this.hiddenUnitNumber; i++) {
-            this.weightsOutputHidden[0][i] += this.learningRate * outputDelta * hiddens[i]
+            this.weightsOutputHidden[0][i] = parseFloat((this.weightsOutputHidden[0][i] + parseFloat((this.learningRate * outputDelta * hiddens[i]).toPrecision(8))).toPrecision(8))
         }
+        //mis à jour des couches caché entre la couche d'entré et celle caché
         for (let i = 0; i < this.hiddenUnitNumber; i++) {
             for (let j = 0; j < this.inputUnitNumber; j++) {
-                this.weightsInputHidden[i][j] += this.learningRate * hiddenDeltas[i] * inputs[j]
+                this.weightsInputHidden[i][j] = parseFloat((this.weightsInputHidden[i][j] +  parseFloat((this.learningRate * hiddenDeltas[i] * inputs[j]).toPrecision(8))).toPrecision(8))
             }
         }
+
+    }
+
+    //fonction d'apprentissage
+    public train(): ErrorPoint[] {
+        let errorPoints: ErrorPoint[] = []
+        
+        for (const { inputs, target } of this.prototype) {
+            const { hidden, output } = this.forwardPropagation(inputs);
+            this.backwardPropagation(inputs, hidden, output, target);
+
+            const nmse = this.calculNMSE([output], [target]);
+            errorPoints.push({ x: nmse, y: this.prototype.indexOf({ inputs, target }) });
+        }
+        //retourne un tableau des erreurs pour chaque prototype
+        return errorPoints
+    }
+
+    //fonction de calcul de l'erreur NMSE
+    private calculNMSE(predictions: number[], targets: number[]): number {
+        let sumError: number = 0;
+        let sumSquareTargets: number = 0;
+
+        for (let i = 0; i < predictions.length; i++) {
+            const error = parseFloat((targets[i] - predictions[i]).toPrecision(8));
+            sumError += parseFloat((error ** 2).toPrecision(8));
+            sumSquareTargets += parseFloat((targets[i] ** 2).toPrecision(8));
+        }
+
+        return parseFloat((sumError / sumSquareTargets).toPrecision(8));
     }
 
     //fonction test prototype
-    getPrototype() {
-        console.log(this.prototype)
+    getPrototype(): Prototype[] {
+        return this.prototype
     }
 
   
